5439d5a1360c21f5649dddcf03d257a1a01ab71a
cyccc95 <cyccc95@naver.com>
2024-01-19 14:55:18 +0900

    first commit

---
+++ src/main/java/com/wemap/api/WemapApiApplication.java
@@ -0,0 +1,13 @@
+package com.wemap.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WemapApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WemapApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wemap/api/WemapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wemap.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WemapApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
